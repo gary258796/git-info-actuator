65b95ab5ea66bfe47335c0c3b22decf52ff15b29
GaryLiao <gary258796@gmail.com>
2022-10-05 00:14:51 +0800

    Init Commit ------------------
    Project Start

---
+++ src/main/java/gary/framework/gitinfoactuator/GitInfoActuatorApplication.java
@@ -0,0 +1,13 @@
+package gary.framework.gitinfoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitInfoActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitInfoActuatorApplication.class, args);
+	}
+
+}
